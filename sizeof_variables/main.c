48ad50eac352e2d0960955cc93610c245b2fd4ba
t3k3 <teke629@gmail.com>
2021-11-15 18:49:13 +0300

    sizeof types

---
+++ sizeof_variables/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    //C version format YYYYMM
+    printf("Version %ld\n", __STDC_VERSION__);
+
+    //Get size of the primitive types on your compiler.
+    printf("sizeof(char)             = %zu bytes\n", sizeof(char));
+    printf("sizeof(_Bool)            = %zu bytes\n", sizeof(_Bool));
+    printf("sizeof(short)            = %zu bytes\n", sizeof(short));
+    printf("sizeof(int)              = %zu bytes\n", sizeof(int));
+    printf("sizeof(long)             = %zu bytes\n", sizeof(long));
+    printf("sizeof(long long)        = %zu bytes\n", sizeof(long long));
+}
