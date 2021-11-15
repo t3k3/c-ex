4dcfcccecffbdd2cbccada07638c5ce396e1bb9b
t3k3 <teke629@gmail.com>
2021-11-15 19:13:26 +0300

    add float types

--- sizeof_variables/main.c
+++ sizeof_variables/main.c
@@ -11,4 +11,7 @@ int main(){
     printf("sizeof(int)              = %zu bytes\n", sizeof(int));
     printf("sizeof(long)             = %zu bytes\n", sizeof(long));
     printf("sizeof(long long)        = %zu bytes\n", sizeof(long long));
+    printf("sizeof(float)            = %zu bytes\n", sizeof(float));
+    printf("sizeof(double)           = %zu bytes\n", sizeof(double));
+    printf("sizeof(long double)      = %zu bytes\n", sizeof(long double));
 }
